4545167e3da01a7dd336b881217e9bcc89689d4b
Elliot Heiner <elliotheiner1997@gmail.com>
2021-08-30 08:56:13 -0600

    Add compound operator examples

--- Operators/Operators.vb
+++ Operators/Operators.vb
@@ -8,11 +8,25 @@
 
         ' Console.WriteLine(2 - 2)
 
-        Console.WriteLine(7 / 3)
+        'Console.WriteLine(7 / 3)
+        'Console.WriteLine(7 \ 3)
+        'Console.WriteLine(7 Mod 3)
 
-        Console.WriteLine(7 \ 3)
+        'Console.WriteLine("hello" & " " & "world")
+        'Dim x As String
+        'For i = 0 To 10
+        '    'x &= "*"
+        '    x = x & "*"
+        '    Console.WriteLine(x)
+        'Next
 
-        Console.WriteLine(7 Mod 3)
+        'Dim y As Integer
+        'For i = 0 To 10
+        '    y += 5
+        '    Console.WriteLine(y)
+        'Next
+
+        Console.WriteLine(16 >= 6)
 
 
         Console.Read()
